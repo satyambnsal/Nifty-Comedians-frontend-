c39023d4e56869a6e2119c298b8b19e1f6b801b1
bigsmall2697 <bigsmall2697@gmail.com>
2021-07-22 15:57:19 -0400

    Fix routing

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express()
 
 app.use(logfmt.requestLogger())
 
-app.use('/', express.static(path.join(__dirname + '/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(Number(process.env.PORT || 5000))
